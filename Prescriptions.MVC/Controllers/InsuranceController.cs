27bff6b781f4cf8570762b3ccf2fd662aaca9212
AiurErb <aiur-erb@web.de>
2024-04-04 22:00:32 +0200

    InsuranceController

---
+++ Prescriptions.MVC/Controllers/InsuranceController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Prescription.DAL.Repos;
+
+namespace Prescriptions.MVC.Controllers
+{
+    public class InsuranceController : Controller
+    {
+        private string? _connectionString;
+
+        public InsuranceController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("SqlServerLocal");
+        }
+        public IActionResult Index()
+        {
+            using SqlConnection connection = new SqlConnection(_connectionString);
+            InsuranceRepo repo = new InsuranceRepo(connection);
+            return View(repo.GetAll());
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
