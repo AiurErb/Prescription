4d0602dd685978ac5c9ea06d7c450bee430cec20
Aiur <Aiur@PC01>
2024-05-01 13:59:45 +0200

    raw data

---
+++ CsvToDbConverter/Classes/Doctor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvToDbConverter.Classes
+{
+    public class Doctor
+    {
+        public long id {  get; set; }
+        public string Anrede {  get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Phones { get; set; }
+        public string Spec {  get; set; }
+
+    }
+
+    public class Address
+    {
+        public string City { get; set; }
+        public string Zip { get; set;}
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public long idOwner { get; set; }
+    }
+}
---
+++ CsvToDbConverter/Utils/AddressHelper.cs
@@ -0,0 +1,41 @@
+﻿
+using CsvToDbConverter.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CsvToDbConverter.Utils
+{
+    public class AddressHelper
+    {
+        public Address Parse(string addressString, long idOwnwer)
+        {
+            Address address = new Address();
+
+            // Define the regular expression pattern to match the address components
+            string pattern = @"^(?<Street>.+) (?<Haus>\S+)(?: - (?<ZIP>\d{5}))? (?<City>.+)$";
+
+            // Match the pattern against the address string
+            Match match = Regex.Match(addressString, pattern);
+
+            if (match.Success)
+            {
+                // Populate the address properties from the matched groups
+                address.Street = match.Groups["Street"].Value;
+                address.Number = match.Groups["Haus"].Value;
+                address.Zip = match.Groups["ZIP"].Value;
+                address.City = match.Groups["City"].Value;
+                address.idOwner = idOwnwer;
+            }
+            else
+            {
+                // Handle the case where the address string does not match the pattern
+                throw new ArgumentException("Invalid address format");
+            }
+            return address;
+        }
+    }
+}
