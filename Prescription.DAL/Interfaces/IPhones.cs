19d40aead88380e4d036890ad83984de6680a3cd
AiurErb <aiur-erb@web.de>
2024-05-01 20:58:37 +0200

    IPhones inteface

---
+++ Prescription.DAL/Interfaces/IPhones.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prescription.DAL.Interfaces
+{
+    public interface IPhones
+    {
+        List<string> Phones { get;set }
+    }
+}
